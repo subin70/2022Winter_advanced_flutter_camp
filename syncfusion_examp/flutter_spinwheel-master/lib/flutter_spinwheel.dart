8467168a2e7cf3a64d7135e539e74614789b6ebf
subin70 <49176709+subin70@users.noreply.github.com>
2021-12-29 09:58:54 +0900

    delete spinwheel

--- syncfusion_examp/flutter_spinwheel-master/lib/flutter_spinwheel.dart
+++
@@ -1,7 +0,0 @@
-library flutter_spinwheel;
-
-export 'src/named_image.dart';
-export 'src/painter_presets.dart';
-export 'src/spinwheel_painter.dart';
-export 'src/spinwheel.dart';
-export 'src/utilities.dart';
